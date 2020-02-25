fdfa0b9748a75e29a5ca9ac3b414aea5dcc5c056
Vladimir Sergeev <sergeev24085@mail.ru>
2020-02-25 12:28:13 +0700

    add data review to server

--- src/admin/store/modules/review.js
+++ src/admin/store/modules/review.js
@@ -2,17 +2,42 @@ import { generateStdError } from "../../helpers/errorHandler";
 
 export default {
   namespaced: true,
+  state: {
+    reviews: []
+  },
+  mutations: {
+    SET_REVIEWS: (state, data) => (state.reviews = data),
+    ADD_REVIEW: (state, review) => state.reviews.push(review)
+  },
   actions: {
-    async addReview(store, review) {
-      const formData = new FormData();
+    // метод добавления нового ревью
+    async addReview({commit}, review) {
+      try {
+        const formData = new FormData();
 
-      //загнали в спец формат, кот. можем передовать данные по сети
-      Object.keys(review).forEach(key => {
-        const value = review[key];
-        formData.append(key, value);
-      });
+        //загнали в спец формат, кот. можем передовать данные по сети
+        Object.keys(review).forEach(key => {
+          const value = review[key];
+          formData.append(key, value);
+        });
+  
+        const response = await this.$axios.post('/reviews', formData);
+        commit('ADD_REVIEW', response.data); 
+        return response;
+      } catch (error) {
+        generateStdError(error);
+      }
 
-      const response = await this.$axios.post('/reviews', formData);
+    },
+    async fetchReviews({ commit }) {
+      // {commit} - метод из store (деструктуризация)
+      try {
+        const response = await this.$axios.get(`/reviews/272`);
+        commit('SET_REVIEWS', response.data.reverse()); // вызовим мутацию и получим ответ в response.data
+        return response;
+      } catch (error) {
+        generateStdError(error);
+      }
     }
   }
 };
